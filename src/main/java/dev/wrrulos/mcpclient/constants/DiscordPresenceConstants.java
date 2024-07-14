689f3d0b07ba60eda139d4747539db21e9373b76
Pedro Agustin Vega <pedroagustinvega04@gmail.com>
2024-07-14 10:19:18 -0300

    Renamed resource files

--- src/main/java/dev/wrrulos/mcpclient/constants/DiscordPresenceConstants.java
+++ src/main/java/dev/wrrulos/mcpclient/constants/DiscordPresenceConstants.java
@@ -2,7 +2,7 @@ package dev.wrrulos.mcpclient.constants;
 
 public class DiscordPresenceConstants {
     // Constants for the Discord presence
-    public static String DLL_PATH = "mcpfiles/discord_game_sdk.dll";
+    public static String DLL_PATH = "mcpfiles/dll/discord_game_sdk.dll";
     public static String MENU_TEXT = "In the main menu";
     public static String PLAYING_TEXT = "Playing on server";
     public static String IMAGE = "mcpclient";
